264f1c25ca31ff09a939292fa74d1d9e60f2dfe1
patGoral <patgoral@gmail.com>
2025-03-20 11:26:43 -0400

    heic?

--- controllers/api/attendees.js
+++ controllers/api/attendees.js
@@ -69,6 +69,7 @@ async function create(req, res, next) {
 				const imagePath = req.files.image[0].path
 				const originalName = req.files.image[0].originalname
 				const fileExtension = path.extname(originalName).toLowerCase() // Get the file extension
+				const newFileName = `${uuid.v4()}.jpg`;
 				const newFilePath = `temp_${originalName}.jpg` // Temporary name for the converted file
 
 				// Check if the image is HEIC
@@ -78,23 +79,24 @@ async function create(req, res, next) {
 						await sharp(imagePath)
 							.toFormat('jpeg')
 							.toFile(newFilePath); // Convert and save to a new file
-	
+			
 						// Upload the converted image to S3
 						const imageParams = {
 							ACL: 'public-read',
 							Bucket: process.env.AWS_BUCKET_NAME,
 							Body: fs.createReadStream(newFilePath),
-							Key: `userImage/${newFilePath}`,
+							Key: `userImage/${newFileName}`, // Use the unique name for S3
 						};
 						const imageData = await s3.upload(imageParams).promise();
-	
+			
 						// Clean up: delete the local converted file
 						fs.unlinkSync(imagePath);
 						fs.unlinkSync(newFilePath);
-	
-						imageUrl = imageData.Location;
+			
+						imageUrl = imageData.Location; // The URL of the uploaded image
 					} catch (err) {
 						console.error('Error during HEIC conversion or S3 upload:', err);
+						// You may want to send an error response to the client
 					}
 				} else {
 					// If it's not a HEIC, upload the original file to S3
